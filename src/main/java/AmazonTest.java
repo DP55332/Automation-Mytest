85a7163181950766a4a33e385c143d3a789f1798
Pooja Patel <test>
2020-12-10 20:44:37 +0530

    My commit

--- src/main/java/AmazonTest.java
+++ src/main/java/AmazonTest.java
@@ -1,10 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 
-public class AmazonLoginOut {
+public class AmazonTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pooja Patel\\Desktop\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
